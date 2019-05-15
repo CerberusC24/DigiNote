8517abb5ac7fce55a508e075899cdcccd9a11243
Christopher Oliveira <chrisoliveira1787@gmail.com>
2019-05-15 11:30:57 -0400

    updated

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const express = require('express');
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
+console.log(`Port: ${PORT}`)
 
 const routes = require("./routes");
 
